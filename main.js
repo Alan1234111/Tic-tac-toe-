b6deb0958c6e733e2a02c9239e90ca2738ebfd35
Alan Pason <alan.pason132@gmail.com>
2023-01-30 22:45:10 +0100

    Add: logic that checks for when the game is over

--- main.js
+++ main.js
@@ -4,19 +4,55 @@
   let tableFields = [];
   let xTurn = true;
 
+  function columnsWin(playerMark) {
+    if ((tableFields[0] == playerMark && tableFields[3] == playerMark && tableFields[6] == playerMark) || (tableFields[1] == playerMark && tableFields[4] == playerMark && tableFields[7] == playerMark) || (tableFields[2] == playerMark && tableFields[5] == playerMark && tableFields[8] == playerMark)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function rowsWin(playerMark) {
+    if ((tableFields[0] == playerMark && tableFields[1] == playerMark && tableFields[2] == playerMark) || (tableFields[3] == playerMark && tableFields[4] == playerMark && tableFields[5] == playerMark) || (tableFields[6] == playerMark && tableFields[7] == playerMark && tableFields[8] == playerMark)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function diagonallyWin(playerMark) {
+    if ((tableFields[0] == playerMark && tableFields[4] == playerMark && tableFields[8] == playerMark) || (tableFields[2] == playerMark && tableFields[4] == playerMark && tableFields[6] == playerMark)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function isWin(mark) {
+    if (columnsWin(mark) || rowsWin(mark) || diagonallyWin(mark)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   function renderGameBoard() {
     for (const field in fields) {
       fields[field].textContent = tableFields[field];
     }
-
   }
 
   function fillField() {
     if (tableFields[this.id - 1]) return;
 
-    xTurn ? (tableFields[this.id - 1] = "X") : (tableFields[this.id - 1] = "O");
+    let mark = "X";
+    xTurn ? (mark = "X") : (mark = "O");
+
     xTurn = !xTurn;
+
+    tableFields[this.id - 1] = mark;
     renderGameBoard();
+    isWin(mark);
   }
 
   fields.forEach((field) => field.addEventListener("click", fillField));
