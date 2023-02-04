43bec534b0e4bac2330d7cc1180c496ea0b40f72
Alan Pason <alan.pason132@gmail.com>
2023-02-04 18:28:37 +0100

    Add: easy mode

--- main.js
+++ main.js
@@ -23,12 +23,14 @@ const gameBoard = (() => {
     [2, 5, 8],
     [0, 4, 8],
   ];
+  let isGameOver = false;
 
   let player1 = playerFactory("player1", "X", false, true);
   let player2 = undefined;
 
   function createSecondPlayer() {
     if (mode == "player") return playerFactory("player2", "O", false, false);
+    if (mode == "easy") return playerFactory("AiEasy", "O", true, false);
   }
 
   function chooseMode() {
@@ -56,9 +58,8 @@ const gameBoard = (() => {
 
   function isTie() {
     const allFilledUp = fields.filter((field) => field.textContent);
-    const isWinField = allFilledUp.some((filledUp) => filledUp.classList.contains("win"));
 
-    if (allFilledUp.length === 9 && !isWinField) {
+    if (allFilledUp.length === 9 && !isGameOver) {
       displayWinner("tie");
       return endGame();
     }
@@ -71,6 +72,7 @@ const gameBoard = (() => {
       if (board[winCombo[0]] == board[winCombo[1]] && board[winCombo[0]] == board[winCombo[2]]) {
         for (let i = 0; i < winCombo.length; i++) {
           fields[winCombo[i]].classList.add("win");
+          isGameOver = true;
         }
 
         displayWinner(board[winCombo[0]]);
@@ -89,22 +91,54 @@ const gameBoard = (() => {
     isTie();
   }
 
-  function fillField() {
-    if (board[this.id - 1]) return;
+  function easyMode(field) {
+    if (field) {
+      board[field.id - 1] = player1.mark;
+      fields[field.id - 1].style.color = "#ee6c4d";
+      renderGameBoard(field);
+    }
 
+    if (isGameOver) return;
+
+    const randomIndex = Math.floor(Math.random() * 9);
+
+    const allFilledUp = fields.filter((field) => field.textContent);
+
+    if (fields[randomIndex].textContent && allFilledUp.length !== 9) {
+      return easyMode();
+    }
+
+    board[randomIndex] = player2.mark;
+    fields[randomIndex].style.color = "#98c1d9";
+
+    setTimeout(() => {
+      renderGameBoard(fields[randomIndex]);
+    }, 200);
+  }
+
+  function playVsComputer(field) {
+    if (mode == "easy") return easyMode(field);
+  }
+
+  function playVsUser(field) {
     if (player1.turn) {
-      board[this.id - 1] = player1.mark;
-      fields[this.id - 1].style.color = "#ee6c4d";
+      board[field.id - 1] = player1.mark;
+      fields[field.id - 1].style.color = "#ee6c4d";
     } else {
-      // ai
-      board[this.id - 1] = player2.mark;
-      fields[this.id - 1].style.color = "#98c1d9";
+      board[field.id - 1] = player2.mark;
+      fields[field.id - 1].style.color = "#98c1d9";
     }
 
     player1.turn = !player1.turn;
     player2.turn = !player2.turn;
 
-    renderGameBoard(this);
+    renderGameBoard(field);
+  }
+
+  function fillField() {
+    if (board[this.id - 1]) return;
+
+    player2.ai == true ? playVsComputer(this) : playVsUser(this);
   }
 
   function reset() {
@@ -115,7 +149,6 @@ const gameBoard = (() => {
     winInfo.classList.add("hide");
 
     fields.forEach((field) => {
-      console.log(field);
       field.classList.remove("win");
       field.style.opacity = "0";
       field.style.pointerEvents = "fill";
@@ -123,6 +156,7 @@ const gameBoard = (() => {
 
     player1.turn = true;
     player2.turn = false;
+    isGameOver = false;
   }
 
   function returnToStart() {
@@ -144,100 +178,3 @@ const gameBoard = (() => {
 
   btnReplay.addEventListener("click", reset);
 })();
-
-//stworzyc 2 zawodnikow w zaleznosi od wybranego trybu
-
-// (function () {
-//   const fields = [...document.querySelectorAll(".field")];
-//   const modes = document.querySelectorAll(".game-options__modes li .btn");
-//   const firstScreen = document.querySelector(".game-options");
-//   const secondScreen = document.querySelector(".board-section");
-//   const btnBack = document.querySelector(".btn-back");
-
-//   let tableFields = [];
-//   let xTurn = true;
-
-//   const playerFactory = (nick, playingMark, isComputer, mode) => {
-//     this.nick = nick;
-//     this.playerMark = playingMark;
-//     this.isComputer = isComputer;
-//     this.mode = mode;
-//   };
-
-//   const playerOne = playerFactory("player", "X", false);
-//   // const playerTwo = null;
-
-//   function chooseMode() {
-//     firstScreen.classList.add("hide");
-//     secondScreen.classList.remove("hide");
-//     const playerTwo = playerFactory("Ai", "O", true, "easy");
-//     console.log(this);
-//   }
-
-//   function returnToStart() {
-//     firstScreen.classList.remove("hide");
-//     secondScreen.classList.add("hide");
-//     tableFields = [];
-//     renderGameBoard();
-//   }
-
-//   function columnsWin(playerMark) {
-//     return (tableFields[0] == playerMark && tableFields[3] == playerMark && tableFields[6] == playerMark) || (tableFields[1] == playerMark && tableFields[4] == playerMark && tableFields[7] == playerMark) || (tableFields[2] == playerMark && tableFields[5] == playerMark && tableFields[8] == playerMark);
-//     if ((tableFields[0] == playerMark && tableFields[3] == playerMark && tableFields[6] == playerMark) || (tableFields[1] == playerMark && tableFields[4] == playerMark && tableFields[7] == playerMark) || (tableFields[2] == playerMark && tableFields[5] == playerMark && tableFields[8] == playerMark)) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   function rowsWin(playerMark) {
-//     return (tableFields[0] == playerMark && tableFields[1] == playerMark && tableFields[2] == playerMark) || (tableFields[3] == playerMark && tableFields[4] == playerMark && tableFields[5] == playerMark) || (tableFields[6] == playerMark && tableFields[7] == playerMark && tableFields[8] == playerMark);
-//     // return true;
-//     // } else {
-//     // return false;
-//     // }
-//   }
-
-//   function diagonallyWin(playerMark) {
-//     return (tableFields[0] == playerMark && tableFields[4] == playerMark && tableFields[8] == playerMark) || (tableFields[2] == playerMark && tableFields[4] == playerMark && tableFields[6] == playerMark);
-//     // if ((tableFields[0] == playerMark && tableFields[4] == playerMark && tableFields[8] == playerMark) || (tableFields[2] == playerMark && tableFields[4] == playerMark && tableFields[6] == playerMark)) {
-//     // return true;
-//     // } else {
-//     // return false;
-//     // }
-//   }
-
-//   function isWin(mark) {
-//     if (columnsWin(mark) || rowsWin(mark) || diagonallyWin(mark)) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   function renderGameBoard(singleField) {
-//     for (const field in fields) {
-//       fields[field].textContent = tableFields[field];
-//     }
-//     if (singleField) singleField.style.opacity = "1";
-//   }
-
-//   function fillField() {
-//     if (tableFields[this.id - 1]) return;
-
-//     let mark = "X";
-//     xTurn ? (mark = "X") : (mark = "O");
-
-//     xTurn = !xTurn;
-
-//     tableFields[this.id - 1] = mark;
-//     renderGameBoard(this);
-//     isWin(mark);
-//   }
-
-//   fields.forEach((field) => field.addEventListener("click", fillField));
-//   modes.forEach((mode) => mode.addEventListener("click", chooseMode));
-//   btnBack.addEventListener("click", returnToStart);
-// })();
-
-// // render contests of gameboard
